b106b991666c58133df49f5695af4724a2c489ff
nicolas.franc <nicolas.franc@0c6e3020-dc5f-4b39-ec15-eab0643b8c4e>
2011-02-15 20:22:35 +0000

    refacto de GuiceServletConfig
    git-svn-id: http://nicolasfrancois.googlecode.com/svn/trunk/PocJerseyGuiceAppEngine@43 0c6e3020-dc5f-4b39-ec15-eab0643b8c4e

--- src/main/java/nfrancois/poc/jerseyguiceappengine/GuiceServletConfig.java
+++ src/main/java/nfrancois/poc/jerseyguiceappengine/GuiceServletConfig.java
@@ -1,5 +1,8 @@
 package nfrancois.poc.jerseyguiceappengine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import nfrancois.poc.jerseyguiceappengine.resource.HelloResource;
 
 import com.google.inject.Guice;
@@ -9,18 +12,20 @@ import com.google.inject.servlet.ServletModule;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
 
 public class GuiceServletConfig extends GuiceServletContextListener {
+	
+	private static final String SDAAS_SERVER_RESOURCES_PACKAGE = HelloResource.class.getPackage().getName();
+	private static final String JERSEY_CONFIG_PROPERTY_PACKAGES = "com.sun.jersey.config.property.packages";	
 
 	@Override
 	protected Injector getInjector() {
-
+		final Map<String, String> params = new HashMap<String, String>();
+		params.put(JERSEY_CONFIG_PROPERTY_PACKAGES, SDAAS_SERVER_RESOURCES_PACKAGE);
+		
 		return Guice.createInjector(new ServletModule() {
 
 			@Override
 			protected void configureServlets() {
-				// Bind classes
-				bind(HelloResource.class);
-
-				serve("/*").with(GuiceContainer.class);
+				serve("/*").with(GuiceContainer.class, params);
 			}
 		});
 	}
